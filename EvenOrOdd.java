aff552b3a1e194f2dd01900872faf721f63284cc
raju dey <rajudey432@gmail.com>
2023-04-12 21:25:27 +0530

    added: Even Or Odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
+        input.close();
+    }
+}

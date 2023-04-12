8b8a2b2500b858f0827c3ddc51702f97cf481a08
raju dey <rajudey432@gmail.com>
2023-04-12 21:20:07 +0530

    added: Quotient Remainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the dividend: ");
+        int dividend = input.nextInt();
+
+        System.out.print("Enter the divisor: ");
+        int divisor = input.nextInt();
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
+
